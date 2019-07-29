9b2b7d95e5c5945b56d52bbb5b532550fe8421c0
Ali Saqib <alisaqib610@hotmail.com>
2019-07-29 12:20:56 -0500

    Configuring types for movies

--- src/rootReducers.js
+++ src/rootReducers.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 
 import user from "./reducers/user";
-import books from "./reducers/movies";
+import movies from "./reducers/movies";
 
 export default combineReducers({
   user,
